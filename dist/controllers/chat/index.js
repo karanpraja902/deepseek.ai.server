4378a85753fb92d777182af134b6814a68e8b17e
karanpraja902 <karanpraja902@gmail.com>
2025-09-06 00:38:38 +0530

    refactor: replace nanoid with UUID for chat ID generation
    - Updated the createChat controller to utilize UUID for generating chat IDs, enhancing consistency and uniqueness in chat identification.

--- dist/controllers/chat/index.js
+++ dist/controllers/chat/index.js
@@ -1,37 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -39,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getChatMessages = exports.deleteAllChats = exports.deleteChat = exports.updateChatTitle = exports.addMessage = exports.getUserChats = exports.getChat = exports.createChat = void 0;
 const Chat_1 = __importDefault(require("../../models/Chat"));
 const express_async_handler_1 = __importDefault(require("express-async-handler"));
+const uuid_1 = require("uuid");
 const chatSchema_1 = require("../../schemas/chatSchema");
 // Create new chat
 exports.createChat = (0, express_async_handler_1.default)(async (req, res) => {
@@ -54,9 +22,8 @@ exports.createChat = (0, express_async_handler_1.default)(async (req, res) => {
             return;
         }
         console.log("Creating chat for userId:", userId);
-        // Dynamic import for nanoid (ES module)
-        const { nanoid } = await Promise.resolve().then(() => __importStar(require('nanoid')));
-        const chatId = nanoid();
+        // Generate UUID for chat ID
+        const chatId = (0, uuid_1.v4)();
         const newChat = new Chat_1.default({
             id: chatId,
             userId: userId,
